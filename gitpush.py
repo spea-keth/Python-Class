0bcbcc217c78ffc6714f2ba5ed080bda1c35d343
spea-keth <itsthatguyjeremy@yahoo.com>
2019-06-27 15:36:30 +0300

    Our 1st Project

--- gitpush.py
+++ gitpush.py
@@ -1,3 +1,4 @@
 print("Introduction to GitHub")
     # git init - initializes/creates a new git repo on the local machine
     # root - staging area - GitHub
+    # git commit - prepare files & folders to push online (must be w/ message)
